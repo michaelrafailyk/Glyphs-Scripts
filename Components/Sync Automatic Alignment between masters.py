dd08b2d9439e1b4d96cd36c733fa9b2dbdd0cc22
Michael Rafailyk <56036489+michaelrafailyk@users.noreply.github.com>
2025-05-15 15:51:39 +0300

    Add files via upload

--- Components/Sync Automatic Alignment between masters.py
+++ Components/Sync Automatic Alignment between masters.py
@@ -1,6 +1,6 @@
 #MenuTitle: Sync Automatic Alignment between masters
 __doc__="""
-Sync Automatic Alignments for components of a current master, if they are automatically aligned in specified reference master (Regular by default).
+Sync Automatic Alignments for components of a current master to specified reference master (Regular by default).
 """
 
 # name of a master to compare the components alignment state
@@ -13,17 +13,17 @@ for master in font.masters:
 		master_reference_id = master.id
 master_selected_id = font.selectedFontMaster.id
 
-Glyphs.font.disableUpdateInterface()
+font.disableUpdateInterface()
 
 # check if the glyph contains components
-for glyph in Glyphs.font.glyphs:
+for glyph in font.glyphs:
 	layer_selected = glyph.layers[master_selected_id]
 	if layer_selected.components:
 		layer_reference = glyph.layers[master_reference_id]
 		for i, component_selected in enumerate(layer_selected.components):
 			component_reference = layer_reference.components[i]
 			# sync component alignment of selected layer to reference layer
-			if component_selected.alignment < 0 and component_reference.alignment >= 0:
+			if component_selected.alignment != component_reference.alignment:
 				component_selected.alignment = component_reference.alignment
 
-Glyphs.font.enableUpdateInterface()
+font.enableUpdateInterface()
