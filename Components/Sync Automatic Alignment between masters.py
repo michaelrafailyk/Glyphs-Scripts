8040ca3ab063957f6cfeae3314794642a4bbe363
Michael Rafailyk <56036489+michaelrafailyk@users.noreply.github.com>
2025-05-18 12:42:20 +0300

    Set reserve reference master if defined one is not exist
    If reference master with defined name is not exist, then the first available master (which is not selected one) will be set as reference master

--- Components/Sync Automatic Alignment between masters.py
+++ Components/Sync Automatic Alignment between masters.py
@@ -3,15 +3,24 @@ __doc__="""
 Sync Automatic Alignments for components of a current master to specified reference master (Regular by default).
 """
 
-# name of a master to compare the components alignment state
+# name of a reference master to compare the components alignment state
 master_reference_name = 'Regular'
 
-# get ids for reference master and for selected master
 font = Glyphs.font
+# get ids for selected master and for reference master
+master_selected_id = font.selectedFontMaster.id
+master_reference_id = False
+# try to find reference master with defined name
 for master in font.masters:
 	if master.name == master_reference_name:
 		master_reference_id = master.id
-master_selected_id = font.selectedFontMaster.id
+		break
+# or get the first available reference master which is not selected one
+if master_reference_id == False or master_reference_id == master_selected_id:
+	for master in font.masters:
+		if master.id != master_selected_id:
+			master_reference_id = master.id
+			break
 
 font.disableUpdateInterface()
 
@@ -22,7 +31,7 @@ for glyph in font.glyphs:
 		layer_reference = glyph.layers[master_reference_id]
 		for i, component_selected in enumerate(layer_selected.components):
 			component_reference = layer_reference.components[i]
-			# sync component alignment of selected layer to reference layer
+			# sync component alignment of selected layer with reference layer
 			if component_selected.alignment != component_reference.alignment:
 				component_selected.alignment = component_reference.alignment
 
